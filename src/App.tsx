e860b3a7b2beaf8bd75a3c79f814f55b429de6d6
Ben Kang <rkdgusdn9@gmail.com>
2020-06-07 02:52:26 +1200

    scss files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { HomePage } from './pages/home';
-
+import React from "react";
+import "./assets/styles/index.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+import { HomePage } from "./pages/home";
+import { NavBar } from "./features/navBar";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Route path="/" component={HomePage} exact />
-			</BrowserRouter>
-		</>
-	);
+    return (
+        <>
+            <NavBar />
+            <BrowserRouter>
+                <Route path="/" component={HomePage} exact />
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
