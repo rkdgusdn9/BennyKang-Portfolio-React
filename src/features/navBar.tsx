19d2c5564b3c39c6b9d6e3e3bcddd4ad5caa050c
Ben Kang <rkdgusdn9@gmail.com>
2020-06-09 03:47:37 +1200

    NavBar update and Hamburger Menu added

--- src/features/navBar.tsx
+++ src/features/navBar.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import logo from "../images/Benny.png";
+import hamburger from "../images/Hamburger.png";
 
 export const NavBar: React.FC = () => {
     const [setActive, setActiveState] = useState("");
@@ -7,5 +9,43 @@ export const NavBar: React.FC = () => {
         setActiveState(setActive === "" ? "active" : "");
     }
 
-    return <nav>test</nav>;
+    return (
+        <nav className="nav-bar">
+            <div className="logo-container">
+                <a href="/" id="logo">
+                    <img src={logo} alt="Home" />
+                </a>
+
+                <div className="hamburger-container">
+                    <a>
+                        <img
+                            className={`hamburger-menu ${setActive}`}
+                            src={hamburger}
+                            onClick={toggleHamburger}
+                        />
+                    </a>
+                </div>
+            </div>
+
+            <div id="navibar" className={`nav-items-container ${setActive}`}>
+                <ul className="list-items">
+                    <li>
+                        <a className="list-items__text">profile</a>
+                    </li>
+                    <li>
+                        <a className="list-items__text">education</a>
+                    </li>
+                    <li>
+                        <a className="list-items__text">skills</a>
+                    </li>
+                    <li>
+                        <a className="list-items__text">experience</a>
+                    </li>
+                    <li>
+                        <a className="list-items__text">contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
