ae6a92a1f0564e9b2cb5f8dce953c424bc1fde3d
Ben Kang <rkdgusdn9@gmail.com>
2020-06-11 04:29:19 +1200

    Footer added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./assets/styles/index.scss";
 import { BrowserRouter, Route } from "react-router-dom";
 import { HomePage } from "./pages/home";
 import { NavBar } from "./features/navBar";
+import { Footer } from "./features/footer";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <BrowserRouter>
                 <Route path="/" component={HomePage} exact />
             </BrowserRouter>
+            <Footer />
         </>
     );
 }
---
+++ src/features/footer.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo1 from "../images/Facebook.png";
+import Logo2 from "../images/LinkedIn.png";
+import Logo3 from "../images/Instagram.png";
+import Logo4 from "../images/Github.png";
+
+export const Footer: React.FC = () => {
+    return (
+        <footer className="footer-container">
+            <div className="footer-head">Social Media</div>
+            <div className="footer-social">
+                <a
+                    href="https://www.facebook.com/Nickie.kang.50"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img className="footer-img" src={Logo1} alt="Facebook" />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/bennykang/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img className="footer-img" src={Logo2} alt="LinkedIn" />
+                </a>
+                <a
+                    href="https://www.instagram.com/rkdgusdn9/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img className="footer-img" src={Logo3} alt="Instagram" />
+                </a>
+                <a
+                    href="https://github.com/rkdgusdn9/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img className="footer-img" src={Logo4} alt="Github" />
+                </a>
+            </div>
+            <div className="footer-final">
+                <div className="footer-name">BEN KANG</div>
+                <div>©Copyright 2020. All right reserved.</div>
+                <div>Powered by Benny Kang</div>
+            </div>
+        </footer>
+    );
+};
