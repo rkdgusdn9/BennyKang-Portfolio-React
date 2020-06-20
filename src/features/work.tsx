4721fe040f95d4568d5e10d1aea6b6832f90c75d
Ben Kang <rkdgusdn9@gmail.com>
2020-06-20 20:48:37 +1200

    Work section changes

--- src/features/work.tsx
+++ src/features/work.tsx
@@ -7,27 +7,63 @@ import workimg4 from "../images/logos/Bannsang.png";
 
 export const Work: React.FC = () => {
     return (
-        <div className="work-container">
+        <div id="work" className="work-container">
             <div className="work-main">
                 <div className="work-main__heading">Work experience</div>
-                <div className="work-main__sub">My work experience</div>
+                <h2 className="work-main__sub">My work experience</h2>
             </div>
             <div className="work-section">
                 <Accordion
                     title={workimg1}
                     content={
                         <div>
-                            bennfaasdfasdfsdafasdfasdfsafasdfsadfsadfsadf
-                            asdfasdfasdfasdfasd
-                            sdafsadfasdfasdfasdfasdfasdfasdfsadfsafasdfadsf
+                            • Worked on IoT software development projects during
+                            a summer internship at Datacom which predominantly
+                            utilised several sensors, Arduino machine, Microsoft
+                            IoT Central, C++ and Microsoft Azure Developer Tools
+                            <br />• Had an experience with Kanban methodology
                         </div>
                     }
                 />
-                <Accordion title={workimg2} content={<div>asdfasdf</div>} />
+                <Accordion
+                    title={workimg2}
+                    content={
+                        <div>
+                            • Worked on Xamarin mobile software development
+                            project during R&amp;D internship at Business
+                            Mechanix for both front-end and back-end with a
+                            database
+                            <br />• Had an experience with SCRUM methodology
+                        </div>
+                    }
+                />
             </div>
             <div className="work-section">
-                <Accordion title={workimg3} content={<div>asdfasdfasdf</div>} />
-                <Accordion title={workimg4} content={<div>asdfsadfsadf</div>} />
+                <Accordion
+                    title={workimg3}
+                    content={
+                        <div>
+                            • Worked as a Table Game Dealer
+                            <br />
+                            • Provided excellent service consistent with the
+                            property's service standards and brand attributes
+                            <br />• Notify supervisor/manager of any suspicious
+                            or unusual activity and/or transactions in a timely
+                            and responsible manner
+                        </div>
+                    }
+                />
+                <Accordion
+                    title={workimg4}
+                    content={
+                        <div>
+                            • Worked as a General Manager
+                            <br />• In charge of hiring people, ordering raw
+                            ingredients, serving customers and alcohol
+                            licensing.
+                        </div>
+                    }
+                />
             </div>
         </div>
     );
